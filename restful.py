4bfd160b7408912669867153245568bf5e873642
MattSom <someszm@gmail.com>
2019-10-17 00:31:27 +0200

    Update

--- restful.py
+++ restful.py
@@ -18,5 +18,8 @@ def returnAll():
 
 @app.route('/lang/<string:name>', methods=['GET'])
 def returnOne(name):
-    langs = [language for language in languages if language['name' == name]]
+    langs = [language for language in languages if language['name'] == name]
     return jsonify({'language ' : langs[0]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
