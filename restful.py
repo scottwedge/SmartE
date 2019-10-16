327ded652ba3fc2919ed03267525fb4658038486
MattSom <someszm@gmail.com>
2019-10-16 19:47:24 +0200

    delete module name check

--- restful.py
+++ restful.py
@@ -20,6 +20,3 @@ def returnAll():
 def returnOne(name):
     langs = [language for language in languages if language['name' == name]]
     return jsonify({'language ' : langs[0]})
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
