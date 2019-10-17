c4e545f731d3e39621d62a4f982f41d94f564e2c
MattSom <someszm@gmail.com>
2019-10-18 01:38:46 +0200

    Update Controller __init__

--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -8,6 +8,6 @@ DB_URI = os.environ.get('CLEARDB_DATABASE_URL')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SECRET KEY'] = ''
-
 app.register_blueprint(root)
+
 db = SQLAlchemy(app)
