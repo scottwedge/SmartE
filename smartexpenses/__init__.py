86bb1703ba62a2526b14436600b4830f88e0074c
MattSom <someszm@gmail.com>
2019-10-17 23:19:04 +0200

    Check env vars

--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from smartexpenses.routes import root
+import os
+
+DB_URI = os.environ.get('CLEARDB_DATABASE_URL')
+print(DB_URI)
 
 app = Flask(__name__)
 app.register_blueprint(root)
