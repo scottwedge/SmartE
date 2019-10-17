40767af83cc7ef992a231e1f6eea0b847466960f
MattSom <someszm@gmail.com>
2019-10-17 22:59:14 +0200

    Update routes

--- smartexpenses/routes.py
+++ smartexpenses/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, abort, make_response, jsonify
 
 root = Blueprint('root', __name__)
 
@@ -19,4 +19,20 @@ def returnAll():
 @root.route('/lang/<string:name>', methods=['GET'])
 def returnOne(name):
     langs = [language for language in languages if language['name'] == name]
+    if not langs:
+        abort(404)
     return jsonify({'language ' : langs[0]})
+
+@root.route('/lang', methods=['POST'])
+def create_language():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    language = {
+        'name' : request.json['name']
+    }
+    languages.append(language)
+    return jsonify({'language': language}), 201
+
+@root.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
