a737edee02ddeb3094ffe2521abcd3708f1c9a26
MattSom <someszm@gmail.com>
2019-10-20 20:22:53 +0200

    Update env handling

--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 ON_HEROKU = 'ON_HEROKU' in os.environ
 
 if ON_HEROKU:
     DB_URL = os.environ.get('CLEARDB_DATABASE_URL')
 else:
+    from dotenv import load_dotenv
+    load_dotenv()
     DB_URL = os.getenv('MYSQL_URL')
 
-print(DB_URL)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
