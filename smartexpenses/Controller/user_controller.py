b141c29f55e00538ef28dde45b489697f4dbca8a
MattSom <someszm@gmail.com>
2019-10-20 19:14:34 +0200

    Configure database connection

--- smartexpenses/Controller/user_controller.py
+++ smartexpenses/Controller/user_controller.py
@@ -1,29 +1,30 @@
 from flask import Blueprint, request, abort, make_response, jsonify
 from smartexpenses.Model.user import User
 
-user_controller = Blueprint('user_controller', __name__)
+user_routes = Blueprint('user_controller', __name__)
 
-@user_controller.route('/user', methods=['GET'])
+@user_routes.route('/user', methods=['GET'])
 def returnAll():
     users = User.query.all()
+    return jsonify({'users': users}), 201
 
-@user_controller.route('/user/<user_id>', methods=['GET'])
+@user_routes.route('/user/<user_id>', methods=['GET'])
 def returnOne(user_id):
     users = [user for user in users if language['id'] == user_id]
     if not users:
         abort(404)
     return jsonify({'user ' : users[0]})
 
-@user_controller.route('/user', methods=['POST'])
+@user_routes.route('/user', methods=['POST'])
 def create_user():
     if not request.json or not 'username' in request.json:
         abort(400)
     user = {
         'username' : request.json['username']
     }
-    users.append(user)
+    User.add(user)
     return jsonify({'user': user}), 201
 
-@user_controller.errorhandler(404)
+@user_routes.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 import os
 
-DB_URI = os.environ.get('CLEARDB_DATABASE_URL')
+load_dotenv()
 
+ON_HEROKU = 'ON_HEROKU' in os.environ
+
+if ON_HEROKU:
+    DB_URL = os.environ.get('CLEARDB_DATABASE_URL')
+else:
+    DB_URL = os.getenv('MYSQL_URL')
+
+print(DB_URL)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-app.config['SECRET KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 from smartexpenses.Controller.root import root
-from smartexpenses.Controller.user_controller import user_controller
+from smartexpenses.Controller.user_controller import user_routes
+       
+db.create_all()
+
 app.register_blueprint(root)
-app.register_blueprint(user_controller)
+app.register_blueprint(user_routes)
+
 
