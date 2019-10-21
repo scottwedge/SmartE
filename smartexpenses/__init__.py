9195ac37ea3f3545350b8731aa08ec17c5100556
MattSom <someszm@gmail.com>
2019-10-21 15:13:40 +0200

    Testing again

--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -6,12 +6,12 @@ ON_HEROKU = 'ON_HEROKU' in os.environ
 
 if ON_HEROKU:
     DB_URL = os.environ.get('CLEARDB_DATABASE_URL')
+    print('HELLOOO')
+    print(DB_URL)
 else:
     from dotenv import load_dotenv
     load_dotenv()
     DB_URL = os.getenv('MYSQL_URL')
-    print('HELLOOO')
-    print(DB_URL)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
