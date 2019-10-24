a281dffbf7d9757d46da0157c660fa405f76ea5d
MattSom <someszm@gmail.com>
2019-10-24 10:38:36 +0200

    Update

--- smartexpenses/Controller/user_controller.py
+++ smartexpenses/Controller/user_controller.py
@@ -8,22 +8,25 @@ def returnAll():
     users = User.query.all()
     return jsonify({'users': users}), 201
 
-@user_routes.route('/user/<user_id>', methods=['GET'])
-def returnOne(user_id):
-    users = [user for user in users if language['id'] == user_id]
-    if not users:
-        abort(404)
-    return jsonify({'user ' : users[0]})
-
-@user_routes.route('/user', methods=['POST'])
-def create_user():
+@user_routes.route('/signup', methods=['GET'])
+def signup():
     if not request.json or not 'username' in request.json:
         abort(400)
     user = {
-        'username' : request.json['username']
+        'username' : request.json['username'],
+        'email' : request.json['email'],
+        'password' : request.json['password'],
+        'admin' : 0,
     }
     User.add(user)
     return jsonify({'user': user}), 201
+
+@user_routes.route('/user/<user_id>', methods=['GET'])
+def returnOne(user_id):
+    users = User.query.all()
+    if not users:
+        abort(404)
+    return jsonify({'user ' : users[0]})
 
 @user_routes.errorhandler(404)
 def not_found(error):
