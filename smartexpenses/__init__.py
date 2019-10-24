33bbf2459ad0423eed5ac9f4b9ba6587c31bc79c
MattSom <someszm@gmail.com>
2019-10-24 17:48:31 +0200

    Update init.py

--- smartexpenses/__init__.py
+++ smartexpenses/__init__.py
@@ -12,25 +12,23 @@ else:
     from dotenv import load_dotenv
     load_dotenv()
     DB_URL = os.getenv('MYSQL_URL')
+    print(DB_URL)
 
 app = Flask(__name__)
 api = Api(app)
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
 @app.before_first_request
 def create_tables():
     db.create_all()
-
-app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
-jwt = JWTManager(app)
-
-app.config['JWT_BLACKLIST_ENABLED'] = True
-app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
