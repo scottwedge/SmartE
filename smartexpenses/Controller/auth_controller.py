46f5b26adac781be052f1adc4b71a28bf05e26ae
MattSom <someszm@gmail.com>
2019-10-30 21:53:27 +0100

    Test token working

--- smartexpenses/Controller/auth_controller.py
+++ smartexpenses/Controller/auth_controller.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse
 from binascii import hexlify
-from flask_jwt_extended import (create_access_token, create_refresh_token, 
-                                jwt_required, jwt_refresh_token_required, 
+from flask_jwt_extended import (create_refresh_token, jwt_required, jwt_refresh_token_required, 
                                 get_jwt_identity, get_raw_jwt)
 from smartexpenses.Model.user import User
 from smartexpenses.Model import RevokedTokenModel
@@ -69,7 +68,6 @@ class AllUsers(Resource):
         return User.delete_all()
       
 class SecretResource(Resource):
-    @jwt_refresh_token_required
     def get(self):
         return {
             'answer': 42
