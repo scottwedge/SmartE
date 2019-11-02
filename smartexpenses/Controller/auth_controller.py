43e3c1bc6576323066cd053d81a9b43ae8ca5494
MattSom <someszm@gmail.com>
2019-11-02 15:00:54 +0100

    Update status response

--- smartexpenses/Controller/auth_controller.py
+++ smartexpenses/Controller/auth_controller.py
@@ -2,8 +2,8 @@ from flask_restful import Resource, reqparse
 from binascii import hexlify
 from flask_jwt_extended import (create_refresh_token, jwt_required, jwt_refresh_token_required, 
                                 get_jwt_identity, get_raw_jwt)
-from smartexpenses.Model.user import User
-from smartexpenses.Model import RevokedTokenModel
+from projectx.Model.user import User
+from projectx.Model import RevokedTokenModel
 
 parser = reqparse.RequestParser()
 parser.add_argument('email', help = 'This field cannot be blank', required = True)
@@ -14,12 +14,14 @@ class UserRegistration(Resource):
         data = parser.parse_args()
 
         if User.find_by_email(data['email']):
-            return {'message': 'User {} already exists'.format(data['email'])}
+            return {
+                'message': 'User {} already exists'.format(data['email']),
+                'status' : 1
+            }
         
         new_user = User(
             email = data['email'],
-            password = User.generate_hash(data['password']),
-            admin = 0
+            password = User.generate_hash(data['password'])
         )
 
         try:
@@ -27,10 +29,14 @@ class UserRegistration(Resource):
             refresh_token = create_refresh_token(identity = data['email'])
             return {
                 'message': 'User {} was created'.format(data['email']),
-                'refresh_token': refresh_token
+                'refresh_token': refresh_token,
+                'status' : 0
                 }
         except Exception as error:
-            return {'message': repr(error)}, 500
+            return {
+                'message': repr(error),
+                'status' : 1
+            }, 500
 
 class UserLogin(Resource):
     def post(self):
@@ -38,16 +44,23 @@ class UserLogin(Resource):
         current_user = User.find_by_email(data['email'])
 
         if not current_user:
-            return {'message': 'User {} doesn\'t exist'.format(data['email'])}
+            return {
+                'message': 'User {} doesn\'t exist'.format(data['email']),
+                'status' : 1    
+            }
         
         if User.verify_hash(data['password'], current_user.password):
             refresh_token = create_refresh_token(identity = data['email'])
             return {
                 'message': 'Logged in as {}'.format(current_user.email),
-                'refresh_token': refresh_token
-                }
+                'refresh_token': refresh_token,
+                'status' : 1
+            }
         else:
-            return {'message': 'Wrong credentials'}
+            return {
+                'message': 'Wrong credentials',
+                'status' : 1
+            }
       
 class UserLogoutRefresh(Resource):
     @jwt_refresh_token_required
@@ -56,9 +69,15 @@ class UserLogoutRefresh(Resource):
         try:
             revoked_token = RevokedTokenModel(jti = jti)
             revoked_token.add()
-            return {'message': 'Refresh token has been revoked'}
-        except:
-            return {'message': 'Something went wrong'}, 500
+            return {
+                'message': 'Successful logout.',
+                'status' : 0
+            }
+        except Exception as error:
+            return {
+                'message': repr(error),
+                'status' : 1
+            }, 500
          
 class AllUsers(Resource):
     def get(self):
@@ -70,4 +89,7 @@ class AllUsers(Resource):
 class SecretResource(Resource):
     @jwt_refresh_token_required
     def get(self):
-        return { 'answer': 42 }, 200
+        return { 
+            'message': 42,
+            'status' : 0
+        }, 200
