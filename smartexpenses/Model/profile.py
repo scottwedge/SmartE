3c69ce97190beba71444fdb24087c18dd48e8730
MattSom <someszm@gmail.com>
2019-11-29 19:12:42 +0100

    refactor profile schema

--- smartexpenses/Model/profile.py
+++ smartexpenses/Model/profile.py
@@ -13,7 +13,7 @@ class Profile(db.Model):
     color =                 db.Column(db.String(100), nullable=False)
     notifications =         db.Column(db.Boolean,     nullable=False)
     num_latest_spendings =  db.Column(db.Integer,     nullable=False)
-    profile_image =         db.Column(db.Text,        nullable=False)
+    image =                 db.Column(db.Text,        nullable=False)
     user_id =               db.Column(db.Integer,     db.ForeignKey('users.id'), unique=True, index=True, nullable=False)
     user =                  db.relationship('User', backref='profiles', lazy=True)
    
@@ -26,7 +26,7 @@ class Profile(db.Model):
         profile = cls.query.filter_by(user_id = user_id).first()
         profile.color = data["color"]
         profile.notifications = bool(data["notifications"])
-        profile.profile_image = bytes(data["profile_image"], encoding='utf8') 
+        profile.image = data["profile_image"], encoding='utf8' 
         profile.num_latest_spendings = data["num_latest_spendings"]
         db.session.commit()
         db.session.close()
@@ -37,7 +37,7 @@ class Profile(db.Model):
         profile = cls.query.filter_by(user_id=user_id).scalar()
         profile.total_spendings = total_spendings
         profile.update_to_db()
-       
+
     @classmethod
     def get_total_spendings(cls,user_id):
         total_spendings = 0
@@ -45,18 +45,18 @@ class Profile(db.Model):
         for expense in expenses:
             total_spendings += expense.value
         return total_spendings
-      
+
     @classmethod
     def return_profile_by_user_id(cls, user_id):
         profile = cls.query.filter_by(user_id = user_id).first()
         if profile:
             return {
                 'user_id' : profile.user_id,
-                'total_spendings' : Profile.call_total_spendings(user_id),
+                'total_spendings' : profile.total_spendings,
                 'color' : profile.color,
                 'notifications' : profile.notifications,
                 'num_latest_spendings' : profile.num_latest_spendings,
-                'profile_image' : json.dumps((profile.profile_image).decode("utf-8")),
-                'privacy_url' : 'https://www.opentracker.net/article/how-write-website-privacy-policy ',
+                'profile_image' : profile.image,
+                'privacy_url' : 'https://www.opentracker.net/article/how-write-website-privacy-policy',
                 'terms_and_conditions_url' : 'https://help.opentracker.net/collection/11-help ' 
             }
